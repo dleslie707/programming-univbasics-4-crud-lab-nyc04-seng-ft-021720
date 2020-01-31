b96eb023a0d1912746c1a49083ac5ddee714e907
spectacular-system-6224 <d.leslie707@gmail.com>
2020-01-31 03:43:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,69 @@
 def create_an_empty_array
-  
+  []
 end
+
+#p create_an_empty_array
+
+puts ""
 
 def create_an_array
-  
+   create_an_empty_array = ["Ashh", "werd", "Dada"]
 end
+
+#p create_an_array
+
+puts ""
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+p add_element_to_end_of_array(create_an_array, "bob")
+puts""
+
+def add_element_to_start_of_array(array, element1)
+  array.unshift(element1) 
 end
+
+p add_element_to_start_of_array(create_an_array, "first lefty")
+puts ""
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
+
+p remove_element_from_end_of_array(create_an_array)
+p remove_element_from_start_of_array(create_an_array)
+puts"" 
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
+
+p retrieve_element_from_index(create_an_array, 1)
+puts""
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
+
+p retrieve_first_element_from_array(create_an_array)
+puts""
 
 def retrieve_last_element_from_array(array)
-  
+  array[2]
 end
+
+p retrieve_last_element_from_array(create_an_array)
+puts""
 
 def update_element_from_index(array, index_number, element)
-
+  array[element]
 end
+
+p update_element_from_index(create_an_array, 0, "fresh bros")
+puts""
